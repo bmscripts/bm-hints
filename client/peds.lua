cb9046f636011a92f6697f39eefc3994b322d01f
Matt <contactbmscripts@gmail.com>
2026-01-23 16:48:15 +0000

    Fixed broker blips to always show

--- client/peds.lua
+++ client/peds.lua
@@ -2,6 +2,28 @@ local spawnedPeds = {}
 local pedTargets = {}
 local pedBlips = {}
 
+-- Create permanent blips for all peds (always visible)
+CreateThread(function()
+    local L = Lang[Config.Locale]
+
+    for index, data in ipairs(Config.Peds) do
+        if data.blip and data.blip.enabled then
+            local blip = AddBlipForCoord(data.coords.x, data.coords.y, data.coords.z)
+
+            SetBlipSprite(blip, data.blip.sprite or 280)
+            SetBlipColour(blip, data.blip.color or 5)
+            SetBlipScale(blip, data.blip.scale or 0.8)
+            SetBlipAsShortRange(blip, true) -- ALWAYS visible
+
+            BeginTextCommandSetBlipName("STRING")
+            AddTextComponentString(data.blip.text or L.general.info_menu_title)
+            EndTextCommandSetBlipName(blip)
+
+            pedBlips[index] = blip
+        end
+    end
+end)
+
 -- Spawn a ped when player enters streaming range
 local function spawnPed(index, data)
     local L = Lang[Config.Locale]
@@ -26,26 +48,11 @@ local function spawnPed(index, data)
 
     spawnedPeds[index] = ped
 
-    -- Optional blip
-    if data.blip and data.blip.enabled then
-        local blip = AddBlipForCoord(data.coords.x, data.coords.y, data.coords.z)
-        SetBlipSprite(blip, data.blip.sprite or 280)
-        SetBlipColour(blip, data.blip.color or 5)
-        SetBlipScale(blip, data.blip.scale or 0.8)
-        SetBlipAsShortRange(blip, true)
-
-        BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString(data.blip.text or L.general.info_menu_title)
-        EndTextCommandSetBlipName(blip)
-
-        pedBlips[index] = blip
-    end
-
     -- Target interaction
     pedTargets[index] = exports.ox_target:addLocalEntity(ped, {
         {
             icon = data.icon,
-            label = data.label or L.general.info_menu_title, data.payment.amount,
+            label = ("%s ($%s)"):format(data.label or L.general.info_menu_title, data.payment.amount),
             onSelect = function()
                 TriggerEvent('bm-hints:openMenu', index)
             end
@@ -67,10 +74,7 @@ local function deletePed(index)
         pedTargets[index] = nil
     end
 
-    if pedBlips[index] then
-        RemoveBlip(pedBlips[index])
-        pedBlips[index] = nil
-    end
+    -- DO NOT REMOVE BLIPS ANYMORE
 end
 
 -- Streaming loop
@@ -110,7 +114,7 @@ CreateThread(function()
             end
         end
 
-        Wait(0) -- smooth marker drawing
+        Wait(0)
     end
 end)
 
